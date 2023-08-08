f6fc5a2dcca008e377f9ada77e1fb4be45cb974d
Preethamn997 <preethamn.sullia.11@gmail.com>
2023-08-09 01:14:00 +0530

    created cart page and some improvement

--- app.py
+++ app.py
@@ -26,12 +26,28 @@ con.execute("""
 """)
 con.close()
 
+con_cart = sqlite3.connect("cart.db")
+con_cart.execute("""
+    CREATE TABLE IF NOT EXISTS cart (
+        id INTEGER PRIMARY KEY,
+        user_id INTEGER,
+        product_id INTEGER,
+        title TEXT,
+        category TEXT,
+        quantity INTEGER,
+        price REAL,
+        image_path TEXT
+    )
+""")
+con_cart.close()
+
+
 # Create the admin login table if it doesn't exist
 con_admin = sqlite3.connect("admin_login.db")
 con_admin.execute("CREATE TABLE IF NOT EXISTS admin_login(pid INTEGER PRIMARY KEY, email TEXT, password TEXT)")
 con_admin.close()
 
-# Create the product details table if doesn't exist
+# Create the product details table if it doesn't exist
 con_products = sqlite3.connect("products.db")
 con_products.execute("CREATE TABLE IF NOT EXISTS products(id INTEGER PRIMARY KEY, title TEXT, price REAL, description TEXT, category TEXT, image_path TEXT)")
 con_products.close()
@@ -51,17 +67,21 @@ def home():
         # Add more product listings here
     ]
     
+    user_email = None
+    logout_button_text = None
+    logout_button_url = None
+    user_profile = None
+
     if 'user_id' in session:
         user_email = session['email']
         logout_button_text = 'Logout'
         logout_button_url = url_for('logout')
-    else:
-        # User is not logged in
-        user_email = None
-        logout_button_text = None
-        logout_button_url = None
 
-    return render_template('home.html', products=products, user_email=user_email, logout_button_text=logout_button_text, logout_button_url=logout_button_url)
+        # Fetch the user profile data from the session
+        user_profile = session.get('user_profile')
+
+    return render_template('home.html', products=products, user_email=user_email, logout_button_text=logout_button_text, logout_button_url=logout_button_url, user_profile=user_profile)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -77,14 +97,21 @@ def login():
         
         if user:
             # User exists, check password
-            if password == user[9]:  # Corrected index to get the password from the query result
+            if password == user[9]:  
                 # Password matches, set session variables
                 session['user_id'] = user[0]
-                session['email'] = user[8]  # Corrected index to get the email from the query result
+                session['email'] = user[8]  
                 
+                # Fetch the user profile data and store it in the session
+                user_profile = get_user_profile(user[0])
+                if user_profile:
+                    session['user_profile'] = user_profile
+
                 flash("Login Successful", "success")
                 con.close()
-                return redirect(url_for('home'))
+                
+                # Redirect back to the page where the login button was clicked previously
+                return redirect(session.pop('next', url_for('home')))
         
         flash("Invalid email or password", "danger")
     
@@ -136,9 +163,11 @@ def logout():
     # Clear session variables
     session.pop('user_id', None)
     session.pop('email', None)
+    session.pop('user_profile', None)
     
     flash("Logged out successfully", "success")
     return redirect(url_for('home'))
+
 
 
 @app.route('/product')
@@ -158,27 +187,33 @@ def product_page():
     product_info = []
 
     for image in images:
-        print(image)
         separator = "\\"
         result = (name + separator + image).lower().replace(" ", "_") # Convert result to lowercase string
-        print(result)
         # Retrieve product information from the database
-        cur.execute("SELECT title, price, description FROM products WHERE image_path = ?", (result,))
+        cur.execute("SELECT id, title, price, description FROM products WHERE image_path = ?", (result,))
         product_data = cur.fetchall()
+        print("product_data", product_data)
 
         if product_data:
             # If a match is found, retrieve the title, price, and description
-            title, price, description = product_data[0]
+            id, title, price, description = product_data[0]
 
             # Add the product information to the list
-            product_info.append({"image": f"{name.lower().replace(' ', '_')}/{image}", "title": title, "price": price, "description": description})
+            product_info.append({"image": f"{name.lower().replace(' ', '_')}/{image}", "title": title, "price": price, "description": description, "pid": id})
 
     # Close the connection
     con.close()
 
-    # Render the template with the combined list of images and product info
-    return render_template('product.html', product_info=product_info)
+    # Fetch the user email and logout button text for rendering
+    user_email = None
+    logout_button_text = None
 
+    if 'user_id' in session:
+        user_email = session['email']
+        logout_button_text = 'Logout'
+
+    # Render the template with the combined list of images and product info
+    return render_template('product.html', product_info=product_info, user_email=user_email, logout_button_text=logout_button_text)
 
 
 @app.route('/add_product', methods=['GET', 'POST'])
@@ -232,17 +267,29 @@ def save_product_image(image, category):
 @app.route('/product_description.html')
 def product_description():
     # Retrieve the product information from the URL parameters
+    product_id = request.args.get('id')
+
     product_image = request.args.get('image')
     product_title = request.args.get('title')
     product_description = request.args.get('description')
     product_price = request.args.get('price')
+    print("product_id", product_id)
+    # Fetch the user email and logout button text for rendering
+    user_email = None
+    logout_button_text = None
 
-    return render_template('product_description.html', 
-                           image=product_image, 
+    if 'user_id' in session:
+        user_email = session['email']
+        logout_button_text = 'Logout'
+    return render_template('product_description.html',
+                           image=product_image,
                            title=product_title,
                            description=product_description,
-                           price=product_price)
-
+                           price=product_price,
+                           user_email=user_email,
+                           logout_button_text=logout_button_text,
+                           product_id=product_id,
+                           )
 
 
 @app.route('/admin', methods=['GET', 'POST'])
@@ -325,33 +372,6 @@ def faq():
     # ...
     return render_template('faq.html')
 
-@app.route('/add_to_cart', methods=['POST'])
-def add_to_cart():
-    # Check if the user is logged in
-    if 'user_id' not in session:
-        # User is not logged in, redirect to the login page
-        flash("Please login to add products to your cart.", "info")
-        return redirect(url_for('login'))
-
-    # Get the product details from the form
-    product_id = request.form.get('product_id')
-    quantity = int(request.form.get('quantity'))
-
-    # Now, you can add the product to the cart or perform any other logic you need
-    # For example, you can store the cart items in the session or a database
-
-    # Here, we'll just show a success message
-    flash(f"Added {quantity} product(s) to cart.", "success")
-
-    # Redirect back to the product description page
-    # Modify this URL to include the necessary parameters as per your setup
-    return redirect(url_for('product_description', product_id=product_id))
-
-@app.route('/cart')
-def cart():
-    # Logic to display the cart items and perform cart operations
-    # ...
-    return render_template('cart.html')
 
 def get_user_profile(user_id):
     con = sqlite3.connect("database.db")
@@ -380,6 +400,111 @@ def profile():
         flash("User profile not found", "danger")
         return redirect(url_for('home'))
 
+@app.route('/update_profile', methods=['POST'])
+def update_profile():
+    # Check if the user is logged in
+    if 'user_id' not in session:
+        flash("Please login to update your profile.", "info")
+        return redirect(url_for('login'))
+
+    # Get the updated profile data from the form
+    user_id = session['user_id']
+    name = request.form['name']
+    address1 = request.form['address1']
+    address2 = request.form['address2']
+    city = request.form['city']
+    pincode = request.form['pincode']
+    state = request.form['state']
+    contact = request.form['contact']
+
+    # Update the user profile data in the database
+    con = sqlite3.connect("database.db")
+    cur = con.cursor()
+    cur.execute("UPDATE customer SET name=?, address1=?, address2=?, city=?, pincode=?, state=?, contact=? WHERE pid=?",
+                (name, address1, address2, city, pincode, state, contact, user_id))
+    con.commit()
+    con.close()
+
+    # Update the session with the new name
+    session['user_profile']['name'] = name
+
+    flash("Profile updated successfully", "success")
+    return redirect(url_for('profile'))
+
+@app.route('/add_to_cart', methods=['POST'])
+def add_to_cart():
+    # Check if the user is logged in
+    if 'user_id' not in session:
+        flash("Please login to add products to your cart.", "info")
+        return redirect(url_for('login'))
+    
+    # Get product details from the form
+    product_id = request.form.get('product_id')
+    quantity = request.form.get('quantity')
+    print(product_id)
+    print(quantity)
+    
+    # Validate product_id and quantity
+    if not product_id or not quantity:
+        flash("Invalid product details", "danger")
+        return redirect(url_for('home'))
+    
+    # Convert product_id and quantity to integers
+    try:
+        product_id = int(product_id)
+        quantity = int(quantity)
+    except ValueError:
+        flash("Invalid product details", "danger")
+        return redirect(url_for('home'))
+    
+    # Get user ID from session
+    user_id = session['user_id']
+    
+    # Retrieve product details from the products table based on the product_id
+    con_products = sqlite3.connect("products.db")
+    cur_products = con_products.cursor()
+    cur_products.execute("SELECT title, category, price, image_path FROM products WHERE id = ?", (product_id,))
+    product_data = cur_products.fetchone()
+    con_products.close()
+    
+    if not product_data:
+        flash("Product not found", "danger")
+        return redirect(url_for('home'))
+    
+    title, category, price, image_path = product_data
+    
+    # Insert the product into the cart table
+    con_cart = sqlite3.connect("cart.db")
+    cur_cart = con_cart.cursor()
+    cur_cart.execute("INSERT INTO cart (user_id, product_id, title, category, quantity, price,image_path) VALUES (?, ?, ?, ?, ?, ?, ?)",
+                     (user_id, product_id, title, category, quantity, price, image_path))
+    con_cart.commit()
+    con_cart.close()
+    
+    flash("Product added to cart", "success")
+    
+    # Redirect back to the referring page (the page where the form was submitted)
+    referring_page = request.headers.get('Referer')
+    return redirect(referring_page)
+
+
+
+@app.route('/cart')
+def cart():
+    if 'user_id' in session:
+        user_id = session['user_id']
+        
+        # Fetch products in the user's cart from the cart table
+        con_cart = sqlite3.connect("cart.db")
+        cur_cart = con_cart.cursor()
+        cur_cart.execute("SELECT id, title, category, quantity, price, image_path FROM cart WHERE user_id = ?", (user_id,))
+        cart_items = cur_cart.fetchall()
+        con_cart.close()
+
+        return render_template('cart.html', cart_items=cart_items)
+    else:
+        flash("Please log in to view your cart", "info")
+        return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
